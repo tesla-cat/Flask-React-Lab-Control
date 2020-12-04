bca4b35b8fcb0f860e8814c5c7ed4bc9273f7b4e
Ricky (Ruiqi) Ding <416640656@qq.com>
2020-12-03 18:37:46 -0800

    ..

--- LabControl/GUI/GUI.py
+++ LabControl/GUI/GUI.py
@@ -5,13 +5,29 @@ from PySimpleGUI.PySimpleGUI import _tray_icon_error, _tray_icon_success
 class Device:
     def __init__(self, serialNumber):
         self.name = 'Device: %d' % serialNumber
-        self.data = {
+        self.settables = {
             'val1': { 'hint': 'type: number, unit: Hz' },
             'val2': { 'hint': 'type: array, example: (1, 2, 3), meaning: (a, b, c)' },
         }
+        self.gettables = self.settables
 
+    def start(self):
+        return 'success', 0
     def setValues(self, dic):
         print('setValues for %s: %s' % (self.name, str(dic)) )
+        return 'success', 0
+    def getValues(self, keys):
+        res = {}
+        for key in keys:
+            res[key] = 'some value'
+        return 'success', res
+    def stop(self):
+        return 'success', 0
+
+statusToIcon = {
+    'success': _tray_icon_success,
+    'error': _tray_icon_error,
+}
 
 class GUI:
     def __init__(self):
@@ -25,42 +41,50 @@ class GUI:
 
     def makeTab(self, dev):
         rows = []
-        for valName, spec in dev.data.items():
-            inputKey = '%s##%s' % (dev.name, valName)
-            setBtnKey = '%s$$%s' % ('set', inputKey)
-            Label = sg.T(valName)
-            Hint = sg.T('    %s' % spec['hint'])
-            Input = sg.In(key = inputKey )
-            SetBtn = sg.Button(button_text='set', key=setBtnKey)
-            rows.append([ Label, Input, SetBtn ])
-            rows.append([ Hint ])
+        groups = { 'set': dev.settables, 'get': dev.gettables }
+        for action, group  in groups.items():
+            for valName, spec in group.items():
+                valKey = '%s!!%s@@%s' % (action, dev.name, valName)
+                btnKey = 'btn##%s' % valKey
+                Label = sg.T(valName)
+                Hint = sg.T('    %s' % spec['hint'])
+                Btn = sg.Button(button_text=action, key=btnKey)
+                Val = sg.In(key = valKey) if action =='set' else sg.T(size=(40,1), key = valKey)
+                rows.append([ Label, Val, Btn ])
+                rows.append([ Hint ])
+        acts = ['start', 'stop']
+        row = [ sg.Button(button_text=act, key='btn##%s' % ('%s!!%s@@null' % (act, dev.name))) for act in acts ]
+        rows.append(row)
         return rows
 
     def run(self):
         App = [[ sg.TabGroup([ self.Tabs.values() ]) ]]
-        window = sg.Window('App', App)
+        self.window = sg.Window('App', App)
         while True:
-            event, values = window.read()
+            event, values = self.window.read()
             if event == sg.WINDOW_CLOSED:
                 break
             self.handle( event, values )
     
     def handle(self, event, values):
-        action, inputKey = event.split('$$')
-        devName, valName = inputKey.split('##')
-        #print(action, devName, valName)
-        val = values[inputKey]
-        if not val: 
-            return
+        _, valKey = event.split('##')
+        action, devVal = valKey.split('!!')
+        devName, valName = devVal.split('@@')
+        print(action, devName, valName)
         if action == 'set':
+            val = values[valKey]
+            if not val: return
             dic = {}
             dic[valName] = eval( val )
             status, result = self.devices[devName].setValues(dic)
-            statusToIcon = {
-                'success': _tray_icon_success,
-                'error': _tray_icon_error,
-            }
-            sg.SystemTray.notify(status, str(result), icon=statusToIcon[status])
+        if action == 'get':
+            status, result = self.devices[devName].getValues([valName])
+            self.window[valKey].update(str(result[valName]))
+        if action == 'start': 
+            status, result = self.devices[devName].start()
+        if action == 'stop':
+            status, result = self.devices[devName].stop()
+        sg.SystemTray.notify(status, str(result), icon=statusToIcon[status])
 
 if __name__ == '__main__':
     dev1 = Device( serialNumber = 1 )
--- LabControl/Instruments/SA124B/SA124B.py
+++ LabControl/Instruments/SA124B/SA124B.py
@@ -7,48 +7,70 @@ class SA124B:
         self.mode = mode
 
         self.name = 'SA124B: %d' % serialNumber
-        self.data = {
+        self.settables = {
             'level': { 'hint': 'type: number, example: -10.0' },
             'center_span': { 'hint': 'type: array, example: (1e9, 100e6), meaning: (center, span)' },
             'IQ': { 'hint': 'type: array, example: (1, 250e3), meaning: (decimation, bandwidth)' },
             'sweep_coupling': { 'hint': 'type: array, example: (10e3, 10e3, 0), meaning: (rbw, vbw, reject)' },
             'acquisition': { 'hint': 'type: array, example: (0, 0), meaning: (detector, scale)' },
         }
+        if mode =='IQ':
+            self.gettables = {
+                'IQ': { 'hint': 'type: array' },
+            }
+        if mode =='sweep':
+            self.gettables = {
+                'sweep': { 'hint': 'type: array' }, 
+            }
     
     def start(self):
-        modes = {'IQ': SA_IQ, 'sweep': SA_SWEEPING}
-        sa_initiate(self.handle, modes[self.mode], 0)
+        try:
+            modes = {'IQ': SA_IQ, 'sweep': SA_SWEEPING}
+            sa_initiate(self.handle, modes[self.mode], 0)
+            return 'success', 0
+        except Exception as e:
+            return 'error', str(e)
 
     def setValues(self, dic):
-        handle = self.handle
-        if 'center_span' in dic:
-            sa_config_center_span(handle, *dic['center_span'])
-        if 'level' in dic:
-            sa_config_level(handle, dic['level'])
-        if 'IQ' in dic:
-            sa_config_IQ(handle, *dic['IQ'])
-        if 'sweep_coupling' in dic:
-            sa_config_sweep_coupling(handle, *dic['sweep_coupling'])
-        if 'acquisition' in dic:
-            sa_config_acquisition(handle, *dic['acquisition'])
-        return 'success', 0
+        try:
+            handle = self.handle
+            if 'center_span' in dic:
+                sa_config_center_span(handle, *dic['center_span'])
+            if 'level' in dic:
+                sa_config_level(handle, dic['level'])
+            if 'IQ' in dic:
+                sa_config_IQ(handle, *dic['IQ'])
+            if 'sweep_coupling' in dic:
+                sa_config_sweep_coupling(handle, *dic['sweep_coupling'])
+            if 'acquisition' in dic:
+                sa_config_acquisition(handle, *dic['acquisition'])
+            return 'success', 0
+        except Exception as e:
+            return 'error', str(e)
     
     def getValues(self, keys = None):
-        if keys == None: keys = self.mode 
-        handle = self.handle
-        res = {}
-        if 'IQ' in keys:
-            IQ = sa_get_IQ_32f(handle)["iq"]
-            res['IQ'] = IQ
-        if 'sweep' in keys:
-            sweep_info = sa_query_sweep_info(handle)
-            sweep = sa_get_sweep_32f(handle)
-            res['sweep_info'] = sweep_info; res['sweep'] = sweep
-        return 'success', res
+        try:
+            if keys == None: keys = self.mode 
+            handle = self.handle
+            res = {}
+            if 'IQ' in keys:
+                IQ = sa_get_IQ_32f(handle)["iq"]
+                res['IQ'] = IQ
+            if 'sweep' in keys:
+                sweep_info = sa_query_sweep_info(handle)
+                sweep = sa_get_sweep_32f(handle)
+                res['sweep_info'] = sweep_info; res['sweep'] = sweep
+            return 'success', res
+        except Exception as e:
+            return 'error', str(e)
 
     def stop(self):
-        handle = self.handle
-        sa_close_device(handle)
+        try:
+            handle = self.handle
+            sa_close_device(handle)
+            return 'success', 0
+        except Exception as e:
+            return 'error', str(e)
 
 if __name__ == '__main__':
     sa124B_IQ = SA124B(serialNumber = 19184645, mode = 'IQ')
